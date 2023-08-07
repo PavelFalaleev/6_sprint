a61c5b2d74f3e8f3cfeebe25759d34bd3ebc1c5e
Pavel Falaleev <pasf@yandex.ru>
2023-08-07 13:34:28 +0300

    финал 6

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -64,9 +64,17 @@ class IceCream(PublishedModel):
     toppings = models.ManyToManyField(Topping)
     is_on_main = models.BooleanField(default=False, verbose_name='На главную')
 
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name='Порядок отображения'
+        )
+
     class Meta:
         verbose_name = 'Мороженое'
         verbose_name_plural = 'Мороженое'
+        ordering = ('output_order', 'title')
 
     def __str__(self):
         return self.title
